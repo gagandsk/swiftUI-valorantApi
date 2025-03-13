a7dc7a01a91edaca9eb9559fec0ac4f3b9f0d112
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-03-13 11:06:44 +0100

    Initial Commit

---
+++ valorantApiTest/valorantApiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  valorantApiTestApp.swift
+//  valorantApiTest
+//
+//  Created by Gagandeep Dass Kaur on 13/3/25.
+//
+
+import SwiftUI
+
+@main
+struct valorantApiTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
